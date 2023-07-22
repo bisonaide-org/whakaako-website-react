d25b051baa49ffdc8688177926d16fd1eeb64e01
bengin34 <becaglar3434@gmail.com>
2023-07-22 15:35:37 +0300

    tailwind css added

--- whakaako-website-react/src/pages/LandingPage.jsx
+++ whakaako-website-react/src/pages/LandingPage.jsx
@@ -1,20 +1,9 @@
-import React from 'react'
-import WhatWeDo from './WhatWeDo'
-import TakeAction from './TakeAction'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import About from './About'
-import Home from './Home'
+
 
 const LandingPage = () => {
   return (
     <>
-     <Navbar/>
-     <Home />
-     <WhatWeDo />
-     <TakeAction />
-        <About />
-        <Footer /> 
+    
     </>
   )
 }
