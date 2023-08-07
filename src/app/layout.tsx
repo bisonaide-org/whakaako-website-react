b8532f5632bc1c6e39e5754c1c40db6a99908cbc
luca <lucacambi77@gmail.com>
2023-08-07 16:10:05 +0200

    nextjs code base

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import NavBar from '@/components/navbar'
+import Footer from "@/app/footer/page";
+
+const inter = Inter({subsets: ['latin']})
+
+export const metadata: Metadata = {
+    title: 'Whakaako',
+    description: 'Whakaako web site',
+}
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <NavBar></NavBar>
+        <main>{children}</main>
+        <Footer></Footer>
+        </body>
+        </html>
+    )
+}
