4421e3fda5ed94bc4e9b9af104b2c837fd9df2c9
busratok <busratokuc@gmail.com>
2023-09-21 21:11:40 +0100

    quote component is created and added to what we do page

--- src/app/whatWeDo/page.tsx
+++ src/app/whatWeDo/page.tsx
@@ -1,15 +1,19 @@
 import Header from "@/components/header/header";
 import React from "react";
 import backgroundImage from "../../../public/photos/02_WhatWeDo/IMG_20230421_121051.jpg";
+import Quote from "@/components/quote";
 
 const WhatWeDo = () => {
   return (
-    <Header
-      backgroundImage={backgroundImage.src}
-      heading="What we do"
-      text="Our nonproﬁt organization is dedicated to offering you high-quality IT education at various locations. In collaboration with our dedicated partners, we provide hands-on training in a group setting."
-      playTitle="course introduction"
-    />
+    <>
+      <Header
+        backgroundImage={backgroundImage.src}
+        heading="What we do"
+        text="Our nonproﬁt organization is dedicated to offering you high-quality IT education at various locations. In collaboration with our dedicated partners, we provide hands-on training in a group setting."
+        playTitle="course introduction"
+      />
+      <Quote />
+    </>
   );
 };
 
