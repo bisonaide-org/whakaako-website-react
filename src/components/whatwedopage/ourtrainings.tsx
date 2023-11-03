cd6083cc72de9be822779d07284baed2855d7232
busratok <busratokuc@gmail.com>
2023-11-03 10:59:29 +0000

    ourtrainings component is updated

--- src/components/whatwedopage/ourtrainings.tsx
+++ src/components/whatwedopage/ourtrainings.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
-import backgroundImage from "../../../public/photos/02_WhatWeDo/P1000639.jpg";
+import backgroundImage from "../../../public/photos/02_WhatWeDo/Background_Academy_features.png";
 import Link from "next/link";
+import { useState } from "react";
 
 const ourTrainingOfferings = [
   {
@@ -8,16 +11,22 @@ const ourTrainingOfferings = [
     heading: "Diverse Course Selection",
     text: " We offer courses at various levels to cater to your individual needs. From beginners to experts, you'll ﬁnd the right course for your IT journey",
     flashGreenIcon: "/photos/02_WhatWeDo/Icons/flashGreenIcon.svg",
+    link: true,
+    linkText: "Course Finder",
+  },
+  {
+    iconPath: "/photos/02_WhatWeDo/Icons/Icon _head _with _light_bulb.svg",
+    heading: "Mentored Courses",
+    text: "We believe that the most effective learning occurs when you can ask questions and receive practical tasks to demonstrate your ability to apply theory. Our experienced IT professionals provide guidance and assistance in this process.",
   },
   {
     iconPath: "/photos/02_WhatWeDo/Icons/Bild2.png",
-    heading: "Practical Training",
-    text: "   We believe in learning by doing. Our courses are hands-on, giving you the opportunity to apply your knowledge in real-world situations.",
-  },
-  {
-    iconPath: "/photos/02_WhatWeDo/Icons/Bild3.png",
-    heading: "Worldwide Expertise",
-    text: "  Our instructors are experienced IT professionals from around the world. They bring international perspectives to the classroom and share their experiences with you.",
+    heading: "Hands-On",
+    text: "We provide hands-on projects, allowing students and graduates to apply their knowledge and gain practical experience, bridging the gap between academia and real-world skills.",
+    flashGreenIcon: "/photos/02_WhatWeDo/Icons/flashGreenIcon.svg",
+    button: true,
+    buttonText: "Training overview",
+    collapsableText: "Currently no trainings open",
   },
   {
     iconPath: "/photos/02_WhatWeDo/Icons/Bild4.png",
@@ -25,7 +34,13 @@ const ourTrainingOfferings = [
     text: " Our goal is to prepare you for the global job market. We provide job search support and connect you with companies worldwide.",
   },
 ];
+
 const OurTrainings = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <section className="relative text-white">
       <div
@@ -38,7 +53,7 @@ const OurTrainings = () => {
       ></div>
       <div className="relative z-10 flex flex-col items-center mx-5 py-10 md:mx-auto md:py-20">
         <h2 className="text-center text-display2 font-display mb-12">
-          Our Training Offerings
+          ACADEMY features
         </h2>
         <div className="flex flex-col gap-16">
           {ourTrainingOfferings.map((item) => (
@@ -56,8 +71,8 @@ const OurTrainings = () => {
               </div>
               <div className=" max-w-lg">
                 <h3 className=" text-display4">{item.heading}</h3>
-                <p>{item.text}</p>
-                {item?.flashGreenIcon && (
+                <p className="text-p">{item.text}</p>
+                {item?.link && (
                   <Link
                     href="#courseFinder"
                     className="flex items-center gap-2"
@@ -68,13 +83,36 @@ const OurTrainings = () => {
                       width={11.46}
                       height={18.46}
                     />
-                    <p>Course Finder</p>
+                    <p className="text-small">{item.linkText}</p>
                   </Link>
+                )}
+                {item?.button && (
+                  <button
+                    className="flex items-center gap-2"
+                    onClick={handleClick}
+                  >
+                    <Image
+                      src={item.flashGreenIcon}
+                      alt="section icon"
+                      width={11.46}
+                      height={18.46}
+                    />
+                    <p className=" text-small">{item.buttonText}</p>
+                  </button>
+                )}
+                {toggle && (
+                  <p className=" pl-5 text-small">{item?.collapsableText}</p>
                 )}
               </div>
             </div>
           ))}
         </div>
+        <Link
+          href="#" //!Go to "Join our community" page when it is ready
+          className="text-white px-3 py-1 mt-3 text-sm font-semibold rounded-lg border-2 border-cyan"
+        >
+          get started
+        </Link>
       </div>
     </section>
   );
