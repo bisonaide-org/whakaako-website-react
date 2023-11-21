f5418698ac231fafc46a639f729beb86d57b3c9e
busratok <busratokuc@gmail.com>
2023-11-21 11:13:29 +0000

    header is fixed according to new style

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -23,29 +23,28 @@ const Header = (props: Props) => {
         className="absolute inset-0"
         style={{ backgroundColor: "rgba(47, 61, 86, 0.75)" }}
       ></div>
-      <div className="relative z-10 min-h-screen max-w-5xl flex flex-col justify-center px-5 pt-24 text-white md:grid md:grid-rows-5 md:grid-flow-col md:gap-6  md:px-0 md:pl-14 md:mx-auto md:pt-0 md:justify-start ">
-        <div className="hidden md:block md:row-start-3 md:row-span-1">
+      <div className="relative z-10 min-h-screen max-w-5xl flex flex-col justify-center px-5 md:p-14  mx-auto ">
+        <div className="flex items-center gap-5">
           <Image
+            className="hidden md:block"
             src={iconOakyardOnDark.src}
             alt="oakyard logo"
-            width={135}
-            height={152}
+            width={150}
+            height={150}
           />
-        </div>
-        <div className="md:row-start-3 md:row-span-3 ">
           <h1 className=" font-display text-yellowGreen text-5xl md:text-7xl mb-6">
             {props.heading}
           </h1>
-          <div className=" md:w-[540px] ">
-            <p className=" text-quote mb-20 whitespace-pre-wrap ">{`${props.text}`}</p>
-          </div>
+        </div>
+        <div className=" max-w-[700px] pl-0 md:pl-[170px]">
+          <p className=" text-white text-quote whitespace-pre-wrap ">{`${props.text}`}</p>
           {props.playTitle && (
-            <div className="flex  justify-start items-center gap-6">
+            <div className="absolute mt-5 text-white flex  justify-start items-center gap-6">
               <div>
                 <Image
                   src={playIcon.src}
-                  width={49.47}
-                  height={49.47}
+                  width={50}
+                  height={50}
                   alt="play icon"
                 />
               </div>
