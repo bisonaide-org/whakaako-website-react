fbc0d76dc0fc4af86390fe9903aa92a4f09affb8
busratok <busratokuc@gmail.com>
2023-12-06 23:31:05 +0000

    header background image and text are added to take action page

--- src/app/takeAction/page.tsx
+++ src/app/takeAction/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import Header from "@/components/header/header";
+import backgroundImage from "../../../public/photos/06_TakeAction/header_background.jpg";
 
 const TakeAction = () => {
   return (
-    <div>
-      <h1>Take Action</h1>
-    </div>
-  )
-}
+    <>
+      <Header
+        backgroundImage={backgroundImage.src}
+        heading="Take Action"
+        text="Ready to make a difference? Explore how you can support our mission, whether by donating, volunteering, or partnering. Your actions shape a brighter future."
+      />
+    </>
+  );
+};
 
-export default TakeAction
+export default TakeAction;
