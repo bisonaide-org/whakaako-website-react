76724cd29766ae180d3dd3f057be7ad58560bb93
busratok <busratokuc@gmail.com>
2023-12-07 13:58:27 +0000

    Inspire Tommorow's leaders components is created

--- src/app/takeAction/page.tsx
+++ src/app/takeAction/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header/header";
 import backgroundImage from "../../../public/photos/06_TakeAction/header_background.jpg";
+import InspireTomorrow from "@/components/takeAction/inspireTomorrow";
 
 const TakeAction = () => {
   return (
@@ -9,6 +10,7 @@ const TakeAction = () => {
         heading="Take Action"
         text="Ready to make a difference? Explore how you can support our mission, whether by donating, volunteering, or partnering. Your actions shape a brighter future."
       />
+      <InspireTomorrow />
     </>
   );
 };
