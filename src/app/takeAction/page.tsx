0bb0f91ac370ed04e31c30f7ccc8dc98cabf2702
busratok <busratokuc@gmail.com>
2023-12-07 14:41:08 +0000

    volunteering component is created

--- src/app/takeAction/page.tsx
+++ src/app/takeAction/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header/header";
 import backgroundImage from "../../../public/photos/06_TakeAction/header_background.jpg";
 import InspireTomorrow from "@/components/takeAction/inspireTomorrow";
+import Volunteering from "@/components/takeAction/volunteering";
 
 const TakeAction = () => {
   return (
@@ -11,6 +12,7 @@ const TakeAction = () => {
         text="Ready to make a difference? Explore how you can support our mission, whether by donating, volunteering, or partnering. Your actions shape a brighter future."
       />
       <InspireTomorrow />
+      <Volunteering />
     </>
   );
 };
