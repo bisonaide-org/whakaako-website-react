aa183e3782823c34ff8d60ab6355935f326fe5b6
busratok <busratokuc@gmail.com>
2023-12-10 00:08:02 +0000

    SupportUsFinanciallyForm is added

--- src/app/takeAction/page.tsx
+++ src/app/takeAction/page.tsx
@@ -3,6 +3,7 @@ import backgroundImage from "../../../public/photos/06_TakeAction/header_backgro
 import InspireTomorrow from "@/components/takeAction/inspireTomorrow";
 import Volunteering from "@/components/takeAction/volunteering";
 import PartnerWithUs from "@/components/takeAction/partnerWithUs";
+import SupportUsFinancallyForm from "@/components/takeAction/supportUsFinanciallyForm";
 
 const TakeAction = () => {
   return (
@@ -12,6 +13,7 @@ const TakeAction = () => {
         heading="Take Action"
         text="Ready to make a difference? Explore how you can support our mission, whether by donating, volunteering, or partnering. Your actions shape a brighter future."
       />
+      <SupportUsFinancallyForm />
       <InspireTomorrow />
       <Volunteering />
       <PartnerWithUs />
