d10173f0446b35953aabf99b15e97cd8e4c7833e
ilkay <06ilkayozturk@gmail.com>
2024-02-22 16:59:03 +0300

    trainings&projects-icons-replaced

--- src/helper/trainingsAndProjects/howItWorksData.tsx
+++ src/helper/trainingsAndProjects/howItWorksData.tsx
@@ -2,7 +2,7 @@ import HowItWorksData from "@/components/forCompaniesPage/types";
 
 export const howItWorksData: HowItWorksData[] = [
   {
-    icon: "/photos/03_ForCompanies/path213.png",
+    icon: "/photos/Shared_Icons/5_find_your_talent/4.svg",
     iconName: "profile icon",
     text: "Please let us know the specific training topics you are interested in. You can select your preferred topics from the list provided in the form below",
     heading: "Ask for training",
@@ -10,7 +10,7 @@ export const howItWorksData: HowItWorksData[] = [
     height: 70,
   },
   {
-    icon: "/photos/03_ForCompanies/Icon_call.png",
+    icon: "/photos/Shared_Icons/5_find_your_talent/5.svg",
     iconName: "phone icon",
     text: "We will prepare a tailored proposal for the requested trainings, offering flexible time slots to suit your specific needs",
     heading: "We contact you",
@@ -18,9 +18,9 @@ export const howItWorksData: HowItWorksData[] = [
     height: 49,
   },
   {
-    icon: "/photos/03_ForCompanies/icon-arrows.png",
+    icon: "/photos/Shared_Icons/5_find_your_talent/6.svg",
     iconName: "cycle icon",
-    text: "We provide both self-paced, mentored trainings and fullytutored sessions, entirely based on your preferences.All our trainings are conducted remotely, featuring experienced trainers and/or mentors. The primary language of instruction is English.",
+    text: "We provide both self-paced, mentored trainings and fully tutored sessions, entirely based on your preferences.All our trainings are conducted remotely, featuring experienced trainers and/or mentors. The primary language of instruction is English.",
     heading: 'Get started"',
     width: 61,
     height: 61,
